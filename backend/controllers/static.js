4db511ddb78e17011e7fea9d922839c54f168dd8
Thierry Prost <thierry.prost@outlook.com>
2018-10-10 14:03:16 +0200

    added style sheet content-type

--- backend/controllers/static.js
+++ backend/controllers/static.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 
 exports.loadStatic = function (req, res) {
   var response = fs.createReadStream(__dirname + '/../../frontend' + req.url, 'utf8');
+  if(req.url.split('.').pop() == 'css') {
+    res.header("content-type", "text/css")
+  }
   response.pipe(res);
 }
 
