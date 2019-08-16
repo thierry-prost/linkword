ce780af977f32991b12aa0589dc16a38ab741cd9
Thierry Prost <40056521+QuantechTP@users.noreply.github.com>
2019-08-16 14:52:13 +0200

    Update linkword.js

--- linkword.js
+++ linkword.js
@@ -2,14 +2,6 @@ var express = require('express');
 var app = express();
 var Server = require('http').Server;
 var server = new Server(app);
-console.log();
-console.log('     ----------------------------------------------------------------------------');
-console.log('\x1b[40m%s\x1b[45m%s\x1b[40m',
-    '    |    You can now use this tool in any browser at ',
-    'http://localhost:3000',
-    '       |');
-console.log('     ----------------------------------------------------------------------------');
-
 server.listen(3000);
 
 require('./backend/routes')(app);
